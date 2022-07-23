808c118a316ba5bef7c204c105cfa36bcd718abb
Josh Klein <kleinjjosh74@gmail.com>
2022-07-23 19:13:04 -0400

    added express/apollo/mongoose and set up basic file structure for schemas/models to launch server

---
+++ server/models/User.js
@@ -0,0 +1,17 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,16 @@
+const User = require("../models/User");
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id})
+                    .select('-__v')
+
+                return userData;
+            }
+        }
+    }
+};
+
+module.exports = resolvers;
