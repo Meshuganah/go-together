6f2993cc779491f61e0a2c9d3e2f3d9a2f0a8e04
Josh Klein <kleinjjosh74@gmail.com>
2022-07-23 19:27:08 -0400

    added models for user and event

--- server/models/User.js
+++ server/models/User.js
@@ -9,8 +9,49 @@ const userSchema = new Schema(
             unique: true,
             trim: true,
         },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email']
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 5
+        },
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        events: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Event'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
     }
 );
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema);
 
