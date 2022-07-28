6917f27bde502c88af0e3d13e50ad97cdcd86f41
Josh Klein <kleinjjosh74@gmail.com>
2022-07-28 17:15:55 -0400

    added API call to find.js

--- client/src/pages/Find.js
+++ client/src/pages/Find.js
@@ -1,15 +1,52 @@
 import Header from "../components/Header";
+import { useState } from 'react';
+//require('dotenv').config();
 
 const Find = () => {
-    
+    const [search, setSearch] = useState('');
+    const [render, setRender] = useState(false);
+    let searchedEvents;
+    const handleClick = async (e) => {
+        try{
+            e.preventDefault();
+            await fetch(`https://api.seatgeek.com/2/events?q=${search}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+            .then(function(response) {
+                if(response.ok) {
+                    response.json().then(function(data) {
+                        searchedEvents = data;
+                        console.log(searchedEvents);
+                        console.log(searchedEvents.events[0].title)
+                        searchedEvents.events.map(eventList => {
+                            console.log(eventList.title);
+                        });
+                    });
+                }
+            })
+        } catch (e) {
+            console.error(e);
+        }
+        setRender(!render);
+    };   
+
+    const handleChange = event => {
+        setSearch(event.target.value);
+    };
+
+
     return(
         <div>
             <Header />
-            <form>
-                <label for="search">Search:</label>
-                <input name="search" id="search" />
-                <button type="submit">Search</button>
-            </form>
+            <div>
+                <input onChange={handleChange} value={search}></input>
+                <button onClick={handleClick} type='submit'>Submit</button>
+                {searchedEvents && 
+                    searchedEvents.events.map(eventList => (
+                        <div>
+                            <span>{eventList.title}</span>
+                        </div>
+                    ))}
+            </div>
+
         </div>
     )
 }
---
+++ server/utils/seat-geek-api.js
@@ -0,0 +1,25 @@
+
+require('dotenv').config();
+
+const { RESTDataSource } = require('apollo-datasource-rest');
+
+class SeatGeekAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://api.seatgeek.com/2/';
+    }
+
+    async getEvent(id) {
+        return this.get(`events/${encodeURIComponent(id)}?client_id=${process.env.CLIENT_ID}`);
+    }
+
+    async getEvents(ids) {
+        return this.get(`events/?id=${ids.join(',')}&client_id=${process.env.CLIENT_ID}`);
+    }
+
+    async searchEvent(query) {
+        return this.get(`events?q=${encodeURIComponent(query.trim())}&client_id=${process.env.CLIENT_ID}`)
+    }
+}
+
+module.exports = SeatGeekAPI;
